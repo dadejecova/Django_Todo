a87f67a1190314dc709f6ccf3c960719b3be6a81
Daniel <danielcoellovargas@gmail.com>
2024-02-25 18:06:42 -0500

    Task function

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from todo.models import Task
 def home(request):
-    tasks = Task.objects.filter(is_completed=False)
+    tasks = Task.objects.filter(is_completed=False).order_by('updated_at')
     context = {
         'tasks':tasks,
     }
